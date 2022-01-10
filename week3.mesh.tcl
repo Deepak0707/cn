eb31343c1b2b41ef28423307b5710f4e71357b4a
Deepak0707 <61240270+Deepak0707@users.noreply.github.com>
2022-01-10 22:59:26 +0530

    Update week3.mesh.tcl

--- week3.mesh.tcl
+++ week3.mesh.tcl
@@ -1,31 +1,73 @@
 set ns [new Simulator]
 
+$ns color 1 Red
+$ns color 2 Blue
+
 set nf [open out.nam w]
 $ns namtrace-all $nf
 
 proc finish {} {
-    global ns nf
-    $ns flush-trace
-    close $nf
-    puts "running nam"
-    exec nam out.nam &
-    exit 0
+	global ns nf
+	$ns flush-trace 
+	close $nf
+	exec nam out.nam &
 }
 
 set n0 [$ns node]
 set n1 [$ns node]
 set n2 [$ns node]
 set n3 [$ns node]
+set n4 [$ns node]
 
-$ns duplex-link $n0 $n1 10Mbps 10ms DropTail
-$ns duplex-link $n0 $n2 10Mbps 10ms DropTail
-$ns duplex-link $n0 $n3 10Mbps 10ms DropTail
+$ns duplex-link $n0 $n1 1Mb 10ms DropTail
+$ns duplex-link $n0 $n2 1Mb 10ms DropTail
+$ns duplex-link $n0 $n3 1Mb 10ms DropTail
+$ns duplex-link $n0 $n4 1Mb 10ms DropTail
 
-$ns duplex-link $n1 $n2 10Mbps 10ms DropTail
-$ns duplex-link $n1 $n3 10Mbps 10ms DropTail
+$ns duplex-link $n1 $n2 1Mb 10ms DropTail
+$ns duplex-link $n1 $n3 1Mb 10ms DropTail
+$ns duplex-link $n1 $n4 1Mb 10ms DropTail
 
-$ns duplex-link $n2 $n3 10Mbps 10ms DropTail
+$ns duplex-link $n2 $n3 1Mb 10ms DropTail
+$ns duplex-link $n2 $n4 1Mb 10ms DropTail
 
+$ns duplex-link $n3 $n4 1Mb 10ms DropTail
 
-$ns at 5.00 "finish"
+$ns duplex-link-op $n0 $n4 orient right
+
+$ns duplex-link-op $n1 $n0 orient right-down
+$ns duplex-link-op $n1 $n2 orient right-up
+$ns duplex-link-op $n2 $n3 orient right-down
+$ns duplex-link-op $n3 $n4 orient left-down
+$ns duplex-link-op $n3 $n2 orient right-down
+
+set udp0 [new Agent/UDP]
+$udp0 set class_ 1
+$ns attach-agent $n0 $udp0
+
+set cbr0 [new Application/Traffic/CBR]
+$cbr0 set packetsize_ 500
+$cbr0 set interval_ 0.005
+$cbr0 attach-agent $udp0
+
+set udp1 [new Agent/UDP]
+$udp1 set class_ 2
+$ns attach-agent $n1 $udp1
+
+set cbr1 [new Application/Traffic/CBR]
+$cbr1 set packetsize_ 500
+$cbr1 set interval_ 0.005
+$cbr1 attach-agent $udp1
+
+set null0 [new Agent/Null]
+$ns attach-agent $n4 $null0
+
+$ns connect $udp0 $null0
+$ns connect $udp1 $null0
+
+$ns at 0.5 "$cbr0 start"
+$ns at 1.0 "$cbr1 start"
+$ns at 1.5 "$cbr0 stop"
+$ns at 2.0 "$cbr1 stop"
+$ns at 2.5 "finish"
 $ns run
